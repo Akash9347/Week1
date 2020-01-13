61793b5d5e0113232ba3f1eb53107dc66906d523
Akash9347 <sing9347@sheridancollege.ca>
2020-01-13 17:37:48 -0500

    just for practicing how github works

---
+++ src/week2/Week2.java
@@ -0,0 +1,22 @@
+/*
+ *  Akashdeep Singh.
+ *  Student ID: 991551849
+ *  
+ */
+package week2;
+
+/**
+ *
+ * @author harman
+ */
+public class Week2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        // TODO code application logic here
+    }
+    
+}
